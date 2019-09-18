af2165212d595917293554b0b4b7b19f527d39bb
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-18 16:32:49 -0600

    added dbconfig file

--- api/server.js
+++ api/server.js
@@ -2,7 +2,10 @@ const express = require('express')
 
 const server = express()
 
+const AuthRouter = require('../auth/auth-router')
+
 server.use(express.json())
+server.use('/auth', AuthRouter)
 
 server.get('/', (req, res) => (
     res.send('sanity check')
---
+++ auth/auth-router.js
@@ -0,0 +1,6 @@
+const router = require(express).Router()
+
+const Users = require('./auth-model')
+
+
+module.exports = router
