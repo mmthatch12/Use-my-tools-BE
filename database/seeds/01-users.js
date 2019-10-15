d4f2e56594f6d101721fc3b3f540f8f8c026756e
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-10-15 11:59:43 -0600

    updated users seed

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -4,5 +4,6 @@ exports.seed = function(knex) {
   return knex('users').insert([
     {username: "dennis", password: bcrypt.hashSync('dennis', 1), first_name: 'Dennis', last_name: 'Baum', email: 'dbaum@gm.com'},
     {username: "sebe", password: bcrypt.hashSync('sebe', 1), first_name: 'Sebe', last_name: 'Bastian', email: 'sebe@gm.com'},
+    {username: "fred", password: bcrypt.hashSync('fred', 1), first_name: 'Fred', last_name: 'Bastian', email: 'fred@gm.com'},
   ])
 };
