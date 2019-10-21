5c04e36717a645d06ea5b372de9b9d2759fe1976
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-10-21 10:26:53 -0600

    tweaked addBTool method, tested, works

--- borrowed-tools/borrowed-tools-model.js
+++ borrowed-tools/borrowed-tools-model.js
@@ -10,12 +10,11 @@ function getBtools(id) {
         .join('users as u', 'b.borrower_id', '=', 'u.id')
         .where({ borrower_id: id })
         .join('tools as t', 'b.tool_id', '=', 't.id')
-        .select('b.borrower_id', 'u.first_name', 'u.last_name', 'b.tool_id', 't.name' )
+        .select('b.borrower_id', 'u.first_name', 'u.last_name', 'b.tool_id', 't.name', 'b.notes' )
 }
 
 function addBTool(body) {
     return db('borrowed_tools as b')
         .join('tools as t', 'b.tool_id', '=', 't.id')
-        .where({ tool_id: id})
         .insert(body)
 }
