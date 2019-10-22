344792b275acb78aae9636df089c5d67db304b95
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-10-22 08:17:44 -0600

    added comments to tools-model

--- tools/tools-model.js
+++ tools/tools-model.js
@@ -9,37 +9,41 @@ module.exports = {
     reqTools
 }
 
+//get all tools owned by owner id
 function getTools(id) {
     return db('tools as t')
         .join('users as u', 't.owner_id', '=', 'u.id')
         .where({ owner_id: id })
-        .select('t.id', 't.name', 'u.first_name', 'u.last_name', 't.owner_id')
+        .select('t.id', 't.name', 'u.first_name', 'u.last_name', 't.owner_id', 't.requested', 't.borrowed')
 }
 
+//get all tools not currently borrowed
 function avTools() {
     return db('tools').where({ borrowed: false })
 }
 
+//add new tool
 function addTool(body) {
     return db('tools')
         .insert(body)
         .then(([tool]) => tool)
 }
 
+//edit tool by tool id
 function editTool(id, body) {
     return db('tools')
         .where({ id })
         .update(body)
-
 }
 
+//delete tool by tool id
 function deleteTool(id) {
     return db('tools')
         .where({ id })
         .del()
 }
 
-//this still need some selection work
+//display all tools that owner id owns that are currently requested
 function reqTools(id) {
     return db('tools as t')
         .join('users as u', 't.owner_id', '=', 'u.id')
