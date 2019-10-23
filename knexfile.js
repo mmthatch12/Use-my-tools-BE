cccd23fc292cd2313f4dbe4c1cdace129574fce8
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-10-23 08:53:24 -0600

    added testing and production to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -21,4 +21,36 @@ module.exports = {
     },
   },
 
+  testing: {
+		client: 'sqlite3',
+		useNullAsDefault: true,
+		connection: {
+			filename: ':memory:'
+		},
+		migrations: {
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/seeds'
+		},
+		pool: {
+			afterCreate: (conn, done) => {
+				// runs after a connection is made to the sqlite engine
+				conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+			}
+		}
+  },
+  
+  production: {
+		client: 'pg',
+		useNullAsDefault: true,
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/seeds'
+		}
+	}
+
 };
